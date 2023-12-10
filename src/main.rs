3fda39803fc55b5f02f1e5cd9b627afdd101a65e
AshGw <AshrefGw@proton.me>
2023-12-10 16:37:39 +0100

    fixed base64_encode fn

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,19 @@ mod utils;
 mod consts;
 use rand::{rngs::OsRng, RngCore};
 use sha2::{Digest, Sha256};
-use base64::{Engine as _, engine::{self, general_purpose}, alphabet};
 
 use utils::{urlsafe_chars,token_urlsafe}; 
-use consts::BASE_64; 
+use consts::base64_encode; 
 
 fn gen_oauth_params() -> (String, String, String, &'static str) {
     let state: String = OsRng.next_u32().to_string();
     let mut code_verifier: [u8; 96] = [0u8; 96];
     OsRng.fill_bytes(&mut code_verifier);
 
-    let code_challenge = BASE_64.encode(&Sha256::digest(&code_verifier));
+    let code_challenge = base64_encode(&Sha256::digest(&code_verifier));
     let code_challenge_method = "S256";
 
-    (state, BASE_64.encode(code_verifier), code_challenge, code_challenge_method)
+    (state, base64_encode(code_verifier), code_challenge, code_challenge_method)
 }
 
 fn gen_csrf_token() -> String {
@@ -34,7 +33,7 @@ fn main() {
     let csrf_token = gen_csrf_token();
     println!("CSRF Token: {}", csrf_token);
     println!("urlsafe chars {}",urlsafe_chars()); 
-    println!("Token_rul safex: {}",token_urlsafe(Some(64)));
+    println!("Token_rul safex: {}",token_urlsafe(Some(96)));
 
 
 }
