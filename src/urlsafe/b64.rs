a9480173e3a74d16b3ca59fffe565dd00f7a4845
Ashref Gwader <ashrefgw@proton.me>
2023-12-17 14:26:31 +0100

    Update b64.rs

--- src/urlsafe/b64.rs
+++ src/urlsafe/b64.rs
@@ -1,5 +1,6 @@
 use base64::{Engine, engine::{general_purpose,GeneralPurpose}, alphabet};
 
+// push this to consts 
 const URLS_B64: GeneralPurpose = GeneralPurpose::new(&alphabet::URL_SAFE, general_purpose::NO_PAD);
 
 pub fn urlsafe_b64encode<T>(token: T) -> String
