b9f927ec5d204139c368c75b3211da43fda551b4
AshGw <AshrefGw@proton.me>
2023-12-17 22:19:26 +0100

    readme added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use oauth2_utils::pkce::PKCE;
+use oauth2_utils::pkce::gen::{gen_code_verifier,gen_code_challenge};
+use oauth2_utils::urlsafe::{urlsafe_token};
+use oauth2_utils::pkce::PKCE; 
+
+
+// run a testing module for all of these 
 
 fn main() {
-    // Create a new PKCE instance and print its code challenge and code verifier
+    let cv =  gen_code_verifier(Some((80))); 
+    println!("{}",cv) ; 
+    println!("{:?}",gen_code_challenge(&"rtrtg"));
+    println!("{}",urlsafe_token(45));
     let pkce = PKCE::new();
-    println!("PKCE Code Challenge: {}", pkce.code_challenge);
-    println!("PKCE Code Verifier: {}", pkce.code_verifier);
-    println!("PKCE Code Verifier: {}", pkce.method);
-
+    println!("{}",pkce.code_challenge);
+    println!("{}",pkce.code_verifier);
 }
